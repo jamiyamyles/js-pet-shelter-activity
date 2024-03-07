7d4cbf95d7a3e46f603fba4cf83b0b962c87c057
JaMiya Myles <jm.myles12@gmail.com>
2024-03-07 14:11:54 -0600

    javascript update

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,65 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Define an array to shelter your pets
- 
-  
-    // Function to add a pet to the shelter - needs to have a param to add a name, type of animal, and age
-    
-  
-    // Function to display pets in the shelter - given to you!
-    function displayPets() {
-      let petList = document.getElementById("pet-list");
-      petList.innerHTML = ""; // Clear the previous pet list
-  
-      // Loop through each pet in the shelter and create a list item for each
-      for(       ){
-      
+var petShelter = [];
 
+// Function to add a pet to the shelter - needs to have a param to add a name, type of animal, and age
+function addPet(petName, petBreed, petAge) {
+  let pet = {
+    name: petName,
+    breed: petBreed,
+    age: petAge,
+  };
+  petShelter.push(pet);
+  console.log(petShelter);
+}
 
-        petList.appendChild(  ); //will assist with adding each pet item to be shown in the list on the browser
-     }
-    }
-    // Add some sample pets to the shelter
-    addPet("Fido", "Dog", 3); //example
-    
-    // Display the pets in the shelter
-  
+// Function to display pets in the shelter
+function displayPets() {
+  let petList = document.getElementById("pet-list");
+  petList.innerHTML = ""; // Clear the previous pet list
+
+  // Loop through each pet in the shelter and create a list item for each
+  for (let i = 0; i < petShelter.length; i++) {
+    let pet = petShelter[i];
+    let petItem = document.createElement(`li`);
+    petItem.textContent = `${pet.name}:${pet.breed}:${pet.age}`;
+    petList.appendChild(petItem); //will assist with adding each pet item to be shown in the list on the browser
+  }
+}
+
+// Add some sample pets to the shelter
+addPet("Fido", "Dog", 3); //example
+addPet("Syvia", "Cat", 7);
+addPet("Rog", "Hamster", 6);
+addPet("Andi", "Dog", 2);
+addPet("Pugo", "Dog", 5);
+
+// Display the pets in the shelter
+displayPets();
 
     //STRETCH GOALS:
     // Function to check if the pet shelter has animals that need homes (based on how many animals are in the shelter)
- 
+    if  (pets=0){
+      return "there are no pets"
+    }
+    else if (pets<=3){
+      return "there are a few pets"
+    }
+    else {
+      "there are many animals"
+    }
+
+   
+   
+  
   
     // Check if the pet shelter has animals that need homes by calling the previous function
-
+    var i= 0
+    if (i>0) {
+      console.log (displayPets);
+    } else {
+      console.log ("No animals to display for adoption")
+    }
   
     // Function to calculate the average age of pets in the shelter
    
